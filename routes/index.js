937ef08efb980f9b738f6e04b1ba3106fa122453
aliprumpung <callmesike@gmail.com>
2018-03-22 21:27:14 +0700

    test connect with psql

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var pg_ = new pg.Client('postgres://callmesike:12345@localhost/firstdb');
 
+const connect = ()=>{
+	pg_.connect((err,res)=>{
+		if (err){
+			console.log('error occured.');
+		}else{
+			console.log('connected to database.');
+		}
+	});
+}
+connect();
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
 });
 
